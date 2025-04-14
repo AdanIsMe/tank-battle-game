83feef11ca0b6137b2521c850191765be0f37744
shaimahoji <127994498+shaimahoji@users.noreply.github.com>
2025-04-15 01:30:19 +0300

    added variables for wait time in backward movement

--- tank.h
+++ tank.h
@@ -16,6 +16,8 @@ private:
     
     Cannon cannon;
     int num_of_shells; // avaliable shells: artillery shells left.
+    int waited = 0; //keep track of steps waiting(2 steps)
+    int shoot_cnt = 0; //keep track of game steps(can't shoot again for 4 steps)
 
     // Tanks position on the game board
     int x_coordinate;
@@ -25,6 +27,7 @@ public:
     // Constructor
     Tank();
 
+    bool isAlive = true;
     // Getters
     int getNumOfShells() const;
 
@@ -32,9 +35,11 @@ public:
     void setNumOfShells();
 
     // Actions
+    void action(int command);
+    void shoot();
     void moveForward();
-
     void moveBackward();
+    
 
     //so its,,,, rotating cannons not tanks, so why put it here? move to class cannon
     //void rotateCannonByEighth(int direction); // 45 degrees: 1 for right, -1 for left
