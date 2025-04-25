a847c652f1b3421b952f6be184797209a2f7fc9c
shaimahoji <127994498+shaimahoji@users.noreply.github.com>
2025-04-25 15:50:58 +0300

    move one step at a time (game manager handles two steps per game step)

--- shell.cpp
+++ shell.cpp
@@ -22,48 +22,48 @@ void Shell::updatePosition() {
     
     //move two steps each game step
     switch(direction) {
-        // UP: Decrease row by 2 (x -= 2)
+        // UP: Decrease row (x--) since rows grow downward
         case Direction::U:
-            x_coordinate = (x_coordinate - 2 + 2 * board_height) % board_height;
+            x_coordinate = (x_coordinate - 1 + board_height) % board_height;
             break;
 
-        // DOWN: Increase row by 2 (x += 2)
+        // DOWN: Increase row (x++)
         case Direction::D:
-            x_coordinate = (x_coordinate + 2) % board_height;
+            x_coordinate = (x_coordinate + 1) % board_height;
             break;
 
-        // LEFT: Decrease column by 2 (y -= 2)
+        // LEFT: Decrease column (y--)
         case Direction::L:
-            y_coordinate = (y_coordinate - 2 + 2 * board_width) % board_width;
+            y_coordinate = (y_coordinate - 1 + board_width) % board_width;
             break;
 
-        // RIGHT: Increase column by 2 (y += 2)
+        // RIGHT: Increase column (y++)
         case Direction::R:
-            y_coordinate = (y_coordinate + 2) % board_width;
+            y_coordinate = (y_coordinate + 1) % board_width;
             break;
 
-        // UP-RIGHT: Decrease row by 2, increase column by 2 (x -= 2, y += 2)
+        // UP-RIGHT: Decrease row, increase column (x--, y++)
         case Direction::UR:
-            x_coordinate = (x_coordinate - 2 + 2 * board_height) % board_height;
-            y_coordinate = (y_coordinate + 2) % board_width;
+            x_coordinate = (x_coordinate - 1 + board_height) % board_height;
+            y_coordinate = (y_coordinate + 1) % board_width;
             break;
         
-        // UP-LEFT: Decrease row by 2, decrease column by 2 (x -= 2, y -= 2)
+        // UP-LEFT: (x--, y--)
         case Direction::UL:
-            x_coordinate = (x_coordinate - 2 + 2 * board_height) % board_height;
-            y_coordinate = (y_coordinate - 2 + 2 * board_width) % board_width;
+            x_coordinate = (x_coordinate - 1 + board_height) % board_height;
+            y_coordinate = (y_coordinate - 1 + board_width) % board_width;
             break;
 
-        // DOWN-RIGHT: Increase row by 2, increase column by 2 (x += 2, y += 2)
+        // DOWN-RIGHT: (x++, y++)
         case Direction::DR:
-            x_coordinate = (x_coordinate + 2) % board_height;
-            y_coordinate = (y_coordinate + 2) % board_width;
+            x_coordinate = (x_coordinate + 1) % board_height;
+            y_coordinate = (y_coordinate + 1) % board_width;
             break;
 
-        // DOWN-LEFT: Increase row by 2, decrease column by 2 (x += 2, y -= 2)
+        // DOWN-LEFT: (x++, y--)
         case Direction::DL: 
-            x_coordinate = (x_coordinate + 2) % board_height;
-            y_coordinate = (y_coordinate - 2 + 2 * board_width) % board_width;
+            x_coordinate = (x_coordinate + 1) % board_height;
+            y_coordinate = (y_coordinate - 1 + board_width) % board_width;
             break;  
     }
 }
