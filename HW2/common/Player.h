7a792f441f002c1dc5595531acb322346765c6e3
AdanIsMe <adanassi@mail.tau.ac.il>
2025-05-12 10:23:43 +0300

    minor cleanups (spaces and enters) of the common package

--- HW2/common/Player.h
+++ HW2/common/Player.h
@@ -1,9 +1,6 @@
 class Player {
 public:
-	Player( int player_index,
-  size_t x, size_t y,
-  size_t max_steps, size_t num_shells ) {}
-	virtual ~Player() {}
-virtual void updateTankWithBattleInfo
-(TankAlgorithm& tank, SatelliteView& satellite_view) = 0;
+    Player(int player_index, size_t x, size_t y, size_t max_steps, size_t num_shells) {}
+    virtual ~Player() {}
+  virtual void updateTankWithBattleInfo(TankAlgorithm& tank, SatelliteView& satellite_view) = 0;
 };
--- HW2/common/SatelliteView.h
+++ HW2/common/SatelliteView.h
@@ -1,5 +1,5 @@
 class SatelliteView {
 public:
 	virtual ~SatelliteView() {}
-virtual char getObjectAt(size_t x, size_t y) const = 0;
+	virtual char getObjectAt(size_t x, size_t y) const = 0;
 };
